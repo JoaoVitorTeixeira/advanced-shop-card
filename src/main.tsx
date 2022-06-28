ee8f3df1d8efd7eb6f9390d16ac8828631582ee9
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2022-06-28 21:57:59 +1000

    fix: change to use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -6,8 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename={import.meta.env.PUBLIC_URL}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
